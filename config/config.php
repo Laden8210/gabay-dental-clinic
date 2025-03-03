161fe50fd02a52a81adebbf659f596610f88c1d7
Laden8210 <domingo.laden@gmail.com>
2025-03-04 01:59:08 +0800

    update

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php
 
-// define('DB_SERVER', 'localhost');
-// define('DB_USERNAME', 'u278537438_gabay_dental');
-// define('DB_PASSWORD', 'Gabay8210');
-// define('DB_NAME', 'u278537438_gabay_dental');
-
-
-
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Laden8210');
-define('DB_NAME', 'gabay_dental_db');
+define('DB_USERNAME', 'u278537438_gabay_dental');
+define('DB_PASSWORD', 'Gabay8210');
+define('DB_NAME', 'u278537438_gabay_dental');
+
+
+
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', 'Laden8210');
+// define('DB_NAME', 'gabay_dental_db');
 
 try {
     $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
