40f2760a4bfd7535e443bac97538be1ee67f8e90
Laden8210 <domingo.laden@gmail.com>
2025-03-03 14:47:39 +0800

    update

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'Laden8210');
-define('DB_NAME', 'gabay_dental_db');
+define('DB_USERNAME', 'u278537438_gabay_dental');
+define('DB_PASSWORD', 'Gabay8210');
+define('DB_NAME', 'u278537438_gabay_dental');
 
 
 try {
